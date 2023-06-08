817f7db695db0466a7276d5377ecbaafd4ddf291
ChaoNEil <neilborgan@gmail.com>
2023-06-08 12:52:35 +0530

    Add grid size changer / modify code as needed

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
-const gridContainer = document.querySelector(".grid-container");
+const gridContainer = document.querySelector('.grid-container');
+const gridSizeInput = document.getElementById('grid-size');
+const updateGridBtn = document.getElementById('update-grid');
 
-//generate grid cells dynamically
-for (let i = 0; i < 16 * 16; i++) {
+let gridSize = 16; // Initial grid size
+
+// Function to generate the grid
+function generateGrid(size) {
+  // Clear existing grid
+  gridContainer.innerHTML = '';
+
+  // grid template columns and rows based on size
+  gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  // Generate grid cells dynamically
+  for (let i = 0; i < size * size; i++) {
     const cell = document.createElement('div');
+    cell.addEventListener('mouseenter', () => {
+      cell.style.backgroundColor = 'black'; //  background color on hover
+    });
     gridContainer.appendChild(cell);
+  }
 }
 
-//hover effect 
-const cells = document.querySelectorAll(".grid-container div");
-cells.forEach(cell => {
-    cell.addEventListener("mouseover", () => {
-        cell.style.backgroundColor = "black";
-    });
+// Initial grid generation
+generateGrid(gridSize);
+
+// Event listener for updating the grid
+updateGridBtn.addEventListener('click', () => {
+  const newSize = parseInt(gridSizeInput.value);
+
+  if (newSize >= 1 && newSize <= 100) {
+    gridSize = newSize;
+    generateGrid(gridSize);
+  } else {
+    alert('Please enter a valid grid size between 1 and 100.');
+  }
 });
+
